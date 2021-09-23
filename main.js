b793e3a942c45da9cdaecf00199fc8876a29f1d6
MatRivard <mat.d.rivard@gmail.com>
2021-09-23 16:45:20 -0400

    General updates throughout

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function closeMobileMenu() {
   document.getElementById("nav-menu-mobile").style.width = "0";
 }
 
+function checkActiveLink() {
+  const links = document.getElementsByClassName("navlink");
+
+  for (var i = 0; i < links.length; i++) {
+    let url = links[i].getAttribute("href").split('/');
+    url = url[url.length - 1]
+    if (window.location.href.indexOf(url) > -1) {
+      links[i].classList.add('active')
+      break;
+    }
+  }
+}
+
+checkActiveLink();
+
