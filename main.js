355cfa67ea517505d5bd4874914ce7952740245c
MatRivard <mat.d.rivard@gmail.com>
2023-04-05 10:40:14 -0400

    Add template social page, parallax script, styles

--- main.js
+++ main.js
@@ -33,4 +33,65 @@ function showSection(sectionId, event) {
   event.target.classList.add("active")
 }
 
+
+// Parallax scroll for projects timeline items
+// const projectTitles = document.querySelectorAll('.project-title');
+const projectTitles = document.querySelectorAll('.project-content');
+let lastScrollY = 0;
+let debounce = false;
+
+
+function onScroll() {
+    lastScrollY = window.scrollY;
+    requestUpdate();
+}
+
+function requestUpdate() {
+  if(!debounce) {
+    requestAnimationFrame(update);
+    debounce = true;
+  }
+}
+
+
+function update() {
+    let projectTitle = null,
+        projectTitleTop = [],
+        projectTitleBottom = [],
+        WindowHeight = window.innerHeight,
+        offset = 0;
+
+  // first loop is going to do all
+  // the reflows (since we use offsetTop)
+    for(let i = 0; i < projectTitles.length; i++) {
+
+        projectTitle = projectTitles[i];
+        projectTitleTop[i] = projectTitle.getBoundingClientRect().top;
+        projectTitleBottom[i] = projectTitle.getBoundingClientRect().bottom;
+    }
+
+  // second loop is going to go through
+  // the projectTitles and update position
+    for(let j = 0; j < projectTitles.length; j++) {
+
+        projectTitle = projectTitles[j];
+        titleTop = projectTitleTop[j];
+        titleBottom = projectTitleBottom[j];
+        itemHalfHeight = (titleBottom - titleTop) / 2;
+        scrollPercent = (WindowHeight - (titleTop + itemHalfHeight)) / WindowHeight;
+
+        if(titleBottom > 0 && titleTop < WindowHeight) {
+          projectTitle.style.transform = 'translateY(' + (40 - 80*scrollPercent) +'px)';
+        } 
+
+    }
+
+  // allow further rAFs to be called
+    debounce = false;
+}
+
+if (window.location.href.includes('social')) {
+  window.addEventListener('scroll', onScroll, false);
+}
+
 checkActiveLink();
